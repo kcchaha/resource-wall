a27b5b66b9a6d0182bc2c0418a0b430139d2754e
adam-brendan <adamcraveiro@gmail.com>
2019-05-23 16:47:08 +0000

    Add cookie-parser and bcrypt dependency, create login route, create lib folder and util folder for js helper functions

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const app         = express();
+const PORT          = process.env.PORT || 8080;
+const ENV           = process.env.ENV || "development";
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const bcrypt        = require('bcrypt');
+const sass          = require("node-sass-middleware");
+const cookieSession = require('cookie-session');
+const app           = express();
 
 const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
