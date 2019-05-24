22fe9e8fef2fb0f5247afb43746ad9b988d36381
adam-brendan <adamcraveiro@gmail.com>
2019-05-24 21:48:13 +0000

    Merge branch 'master' of github.com:kcchaha/resource-wall

--- db/seeds/01_insert_new_users.js
+++ db/seeds/01_insert_new_users.js
@@ -5,14 +5,17 @@ exports.seed = function (knex, Promise) {
       return Promise.all([
         // Inserts seed entries
         knex('user_credentials').insert({
+          id: 1,
           email: 'kcchaha@gmail.com',
           password: '123'
         }),
         knex('user_credentials').insert({
+          id: 2,
           email: 'gizem_ocak@outlook.com',
           password: '123'
         }),
         knex('user_credentials').insert({
+          id: 3,
           email: 'adamcraveiro@gmail.com',
           password: '123'
         })
