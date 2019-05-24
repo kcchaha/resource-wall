7272cb3f097ec3b2ef06f6c38f8ea9d61fd6035c
gizemocak <gizem_ocak@outlook.com>
2019-05-24 05:05:23 +0000

    fully functioning load links and search features

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,23 +27,47 @@ function login() {
   })
 }
 
+//search bar-> get links
 function getLinks() {
+  //hide the present links
+  //show searched link
   $('#search button').on('click', function (event) {
     event.preventDefault();
     const inputText = $('#search input').val()
     if (inputText) {
+      $('#link-container').empty()
       $.ajax({
         method: "GET",
         url: `/links?key=${inputText}`
       }).done(function (links) {
-        console.log(links)
+        addLinksToPage(links)
       })
     }
   })
 }
 
+//load links
+function loadLinks() {
+  $.ajax({
+    method: "GET",
+    url: "/links"
+  }).done(function (links) {
+    $('#link-container').empty()
+    addLinksToPage(links)
+    console.log(links)
+  })
+}
+
+function addLinksToPage(links) {
+  links.forEach(link => {
+    $('#link-container').append(`<div class='one-link'>${link.title}, ${link.url}, ${link.description}}</div>`)
+  })
+}
+
 $(document).ready(function () {
+  loadLinks();
   getLinks();
   register();
   login();
+
 })
