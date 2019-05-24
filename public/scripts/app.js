ca7581c8755251a6fba1045ca0bdd5b9236e8fa7
Gizem Ocak <gizem_ocak@outlook.com>
2019-05-24 14:40:31 -0700

    Allow users to create links
    * create /links route and make request to database for links
    
    * create get links function to display the links
    
    * fully functioning load links and search features
    
    * change input type to password in update-profile
    
    * update links route to search for description
    
    * installed ogs to get image urls
    
    * use ogs to show image urls
    
    * make an ajax request to backend to create a new link
    
    * required email and password
    
    * fully functioning links routes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,15 +5,87 @@ function register() {
       url: '/register',
       type: 'POST',
       data: $(this).serialize()
-    }).done(function (error) {
+    }).done(function () {
       window.location.replace('/')
     }).fail(err => {
-      $('.is-member span').text('This email already exists')
+      $('.is-member span.member').text('This email already exists')
+    })
+  })
+}
+
+function login() {
+  $("#sign-in-form").on("submit", function (event) {
+    event.preventDefault();
+    console.log("This is working!")
+    $.ajax({
+      method: "POST",
+      url: $(this).attr("action"),
+      data: $(this).serialize()
+    }).done(function () {
+      window.location.replace('/')
+    })
+  })
+}
+
+//search bar-> get links
+function getLinks() {
+  //hide the present links
+  //show searched link
+  $('#search button').on('click', function (event) {
+    event.preventDefault();
+    const inputText = $('#search input').val()
+    if (inputText) {
+      $('#link-container').empty()
+      $.ajax({
+        method: "GET",
+        url: `/links?key=${inputText}`
+      }).done(function (links) {
+        console.log(links)
+        addLinksToPage(links)
+      })
+    }
+  })
+}
+
+//load links
+function loadLinks() {
+  $.ajax({
+    method: "GET",
+    url: "/links"
+  }).done(function (links) {
+    $('#link-container').empty()
+    console.log('ll', links)
+    addLinksToPage(links)
+  })
+}
+
+function addLinksToPage(links) {
+  //add category
+  //create a category object to assign icons to the links
+  links.forEach(link => {
+    $('#link-container').prepend(`<div class='one-link'><img src=${link.imgUrl}></img>, ${link.title}, ${link.url}, ${link.description}}</div>`)
+  })
+}
+
+//create a new link
+function createLink() {
+  $('#newLink').on('submit', function () {
+    event.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: '/links',
+      data: $(this).serialize()
+    }).done(function () {
+      window.location.replace('/')
     })
   })
 }
 
 $(document).ready(function () {
+  loadLinks();
+  getLinks();
   register();
   login();
+  createLink();
+
 })
